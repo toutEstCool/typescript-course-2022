f754c6940b20cd50bc561931a0e04a5194700c4f
Danil Panarin <danilapanarin96@gmail.com>
2022-11-14 13:06:22 +0600

    add: linteral-types

--- app.js
+++ app.js
@@ -1,27 +1,5 @@
 "use strict";
-/*{ Union Type }*/
-function logId(id) {
-    /*{ Сужение типов}*/
-    if (typeof id === 'string') {
-        console.log(id.toLocaleUpperCase());
-    }
-    else if (typeof id === 'number') {
-        console.log(id.valueOf());
-    }
-    else {
-        console.log(id);
-    }
-}
-function logError(err) {
-    if (Array.isArray(err)) {
-        console.log(err);
-    }
-    else {
-        console.log(err);
-    }
-}
-function logObject(obj) {
-    if ('a' in obj) {
-        console.log(obj.a);
-    }
+/*{  Lenteral Type }*/
+function fetchWithAuth(url, method) {
+    return 1;
 }
--- app.ts
+++ app.ts
@@ -1,26 +1,7 @@
-/*{ Union Type }*/
-function logId(id: string | number | boolean) {
-  /*{ Сужение типов}*/
-  if (typeof id === 'string') {
-    console.log(id.toLocaleUpperCase());
-  } else if (typeof id === 'number') {
-    console.log(id.valueOf());
-  } else {
-    console.log(id);
-  }
+/*{  Lenteral Type }*/
+function fetchWithAuth(url: string, method: 'get' | 'post'): 1 | -1 {
+  return 1;
 }
 
-function logError(err: string | string[]) {
-  if (Array.isArray(err)) {
-    console.log(err);
-  } else {
-    console.log(err);
-  }
-}
 
-function logObject(obj: { a: number } | { b: number }) {
-  if ('a' in obj) {
-    console.log(obj.a);
 
-  }
-}
