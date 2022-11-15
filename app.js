58fff5a9849cae7f7588d206406cd4b9887a7faa
Danil Panarin <danilapanarin96@gmail.com>
2022-11-15 13:51:42 +0600

    add: interface

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ const user = {
     name: 'Danil',
     age: 26,
     skills: ['1', '2'],
-    id: 2,
+    roleId: 1,
+    createdAt: new Date(),
+    log: id => '',
 };
--- app.ts
+++ app.ts
@@ -1,18 +1,27 @@
 // Type Aliases
 
-type User = {
+interface User {
 	name: string
 	age: number
 	skills: string[]
+
+	log: (id: number) => string
 }
-type Role = {
-	id: number
+
+interface Role {
+	roleId: number
 }
-// Intersection
-type UserWithRole = User & Role
+
+interface UserWithRole extends User, Role {
+	createdAt: Date
+}
+
 const user: UserWithRole = {
 	name: 'Danil',
 	age: 26,
 	skills: ['1', '2'],
-	id: 2,
+	roleId: 1,
+	createdAt: new Date(),
+
+	log: id => '',
 }
